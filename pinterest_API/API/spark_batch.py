6c18c38f1065ae820b69997a260e8a810340928f
Patrick Govus <patrickgovus99@gmail.com>
2023-02-15 12:33:49 +0000

    spark installed (eventually)

---
+++ pinterest_API/API/spark_batch.py
@@ -0,0 +1,28 @@
+# import findspark
+# findspark.find()
+# findspark.init()
+import pyspark
+import multiprocessing
+
+
+cfg = (
+    pyspark.SparkConf()
+    # Setting the master to run locally and with the maximum amount of cpu coresfor multiprocessing.
+    .setMaster(f"local[{multiprocessing.cpu_count()}]")
+    # Setting application name
+    .setAppName("TestApp")
+    # Setting config value via string
+    .set("spark.eventLog.enabled", False)
+    # Setting environment variables for executors to use
+    .setExecutorEnv(pairs=[("VAR3", "value3"), ("VAR4", "value4")])
+    # Setting memory if this setting was not set previously
+    .setIfMissing("spark.executor.memory", "1g")
+)
+
+# Getting a single variable
+# print(cfg.get("spark.executor.memory"))
+# Listing all of them in string readable format
+# print(cfg.toDebugString())
+session = pyspark.sql.SparkSession.builder.config(conf=cfg).getOrCreate()
+rddDistributedData = session.sparkContext.parallelize([1, 2, 3, 4, 5])
+print(rddDistributedData.collect())
