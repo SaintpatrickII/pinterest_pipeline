122074ab09b00045a3d6bfd56143cfdd09b2729e
Patrick Govus <patrickgovus99@gmail.com>
2023-03-13 11:33:40 +0000

    consumer stream run & cleans batch, no upload to postgres yet

--- pinterest_API/API/kafka_consumer_stream.py
+++ pinterest_API/API/kafka_consumer_stream.py
@@ -19,12 +19,12 @@ from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 import os
 import json
 import sys
-spark_path = '/Users/paddy/spark/spark-3.3.1-bin-hadoop3' # spark installed folder
-os.environ['SPARK_HOME'] = spark_path
-sys.path.insert(0, spark_path + "/bin")
-sys.path.insert(0, spark_path + "/python/pyspark/")
-sys.path.insert(0, spark_path + "/python/lib/pyspark.zip")
-sys.path.insert(0, spark_path + "/python/lib/py4j-0.10.9.5-src.zip")
+# spark_path = '/Users/paddy/spark/spark-3.3.1-bin-hadoop3' # spark installed folder
+# os.environ['SPARK_HOME'] = spark_path
+# sys.path.insert(0, spark_path + "/bin")
+# sys.path.insert(0, spark_path + "/python/pyspark/")
+# sys.path.insert(0, spark_path + "/python/lib/pyspark.zip")
+# sys.path.insert(0, spark_path + "/python/lib/py4j-0.10.9.5-src.zip")
 # %%
 os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1,org.postgresql:postgresql:42.2.10 pyspark-shell'
 postgres_ps = os.environ['POSTGRES_PASSWORD']
@@ -71,11 +71,35 @@ stream_df = stream_df.withColumn("follower_count",regexp_replace(col("follower_c
 stream_df = stream_df.withColumn("follower_count",regexp_replace(col("follower_count"), "M", "000000"))
 
 
-def foreach_batch_function(df, epoch_id):
-    url = 'jdbc:postgresql://localhost:5432/pinterest_streaming'
-    properties = {"user": "postgres", "password": postgres_ps, "driver": "org.postgresql.Driver"}
-    df.write.jdbc(url=url, table="experimental_data", mode="append", properties=properties)
+# def foreach_batch_function(df, epoch_id):
+#     url = 'jdbc:postgresql://localhost:5432/pinterest_streaming'
+#     properties = {"user": "postgres", "password": postgres_ps, "driver": "org.postgresql.Driver"}
+#     df.write.jdbc(url=url, table="experimental_data", mode="append", properties=properties)
 
-# ds = stream_df.writeStream.outputMode('update').format('console').start().awaitTermination()   
-stream_df.writeStream.format("jdbc").foreachBatch(foreach_batch_function).outputMode('update').format('console').start().awaitTermination() 
+# # ds = stream_df.writeStream.outputMode('update').format('console').start().awaitTermination()   
+# stream_df.writeStream.format("jdbc").foreachBatch(foreach_batch_function).outputMode('update').format('console').start().awaitTermination() 
+
+
+
+def _write_streaming(
+    df,
+    epoch_id
+) -> None:         
+
+    df.write \
+        .mode('append') \
+        .format("jdbc") \
+        .option("url", "jdbc:postgresql://localhost:5432/pinterest_streaming") \
+        .option("driver", "org.postgresql.Driver") \
+        .option("dbtable", 'experimental_data') \
+        .option("user", 'postgres') \
+        .option("password", postgres_ps) \
+        .save() 
+
+stream_df.writeStream \
+    .foreachBatch(_write_streaming) \
+    .outputMode('update') \
+    .format('console') \
+    .start() \
+    .awaitTermination()
 #%%
