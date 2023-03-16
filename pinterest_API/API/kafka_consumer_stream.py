6b7f55dbe05e72b62c039da8ba2416426ecdc58f
Patrick Govus <patrickgovus99@gmail.com>
2023-03-16 09:33:48 +0000

    streaming needed extra space

--- pinterest_API/API/kafka_consumer_stream.py
+++ pinterest_API/API/kafka_consumer_stream.py
@@ -19,17 +19,17 @@ from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 import os
 import json
 import sys
-spark_path = '/Users/paddy/spark/spark-3.3.1-bin-hadoop3' # spark installed folder
+# spark_path = '/Users/paddy/spark/spark-3.3.1-bin-hadoop3' # spark installed folder
 # os.environ['SPARK_HOME'] = spark_path
 # sys.path.insert(0, spark_path + "/bin")
 # sys.path.insert(0, spark_path + "/python/lib/")
 # sys.path.insert(0, spark_path + "/python/lib/pyspark.zip")
-sys.path.insert(0, spark_path + "/python/lib/py4j-0.10.9.5-src.zip")
+# sys.path.insert(0, spark_path + "/python/lib/py4j-0.10.9.5-src.zip")
 # %%
 # os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.13:3.3.1,org.apache.spark:spark-sql_2.13:3.3.1,org.postgresql:postgresql:42.3.3 pyspark-shell'
 # os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.13:3.3.1,org.postgresql:postgresql:42.2.10 pyspark-shell'
 
-os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.1 pyspark-shell'
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.1, org.postgresql:postgresql:42.3.3 pyspark-shell'
 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
@@ -46,6 +46,7 @@ stream_topic_name = 'streamTopic'
 spark = SparkSession \
     .builder \
     .appName('kafkaStreaming') \
+    .config("spark.driver.memory", "15g") \
     .getOrCreate()
 
 
@@ -85,7 +86,7 @@ stream_df = stream_df.withColumn("follower_count",regexp_replace(col("follower_c
 #     properties = {"user": "postgres", "password": postgres_ps, "driver": "org.postgresql.Driver"}
 #     df.write.jdbc(url=url, table="experimental_data", mode="append", properties=properties)
 
-ds = stream_df.writeStream.outputMode('update').format('console').start().awaitTermination()   
+# ds = stream_df.writeStream.outputMode('update').format('console').start().awaitTermination()   
 #%%
 # stream_df.writeStream.format("jdbc").foreachBatch(foreach_batch_function).outputMode('update').format('console').start().awaitTermination() 
 
