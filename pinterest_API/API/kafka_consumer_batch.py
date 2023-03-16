822eceb79c390048be735b023bcf20469639b990
Patrick Govus <patrickgovus99@gmail.com>
2023-03-16 12:46:05 +0000

    docs updated

--- pinterest_API/API/kafka_consumer_batch.py
+++ pinterest_API/API/kafka_consumer_batch.py
@@ -17,14 +17,18 @@ from pydantic import Json
 
 
 
+# Creating a connection to S3
 s3 = boto3.resource('s3')
 s3_client = boto3.client('s3')
 
+# Creating a KafkaAdminClient object that will be used to create a topic
 admin_client = KafkaAdminClient(
     bootstrap_servers='localhost:9092',
     client_id='KafkatoPython'
 )
 
+
+# Creating a Kafka consumer that will read from the topic `batchtest`
 consumer_batch = KafkaConsumer(
     'batchtest',
     bootstrap_servers='localhost:9092',
@@ -35,17 +39,33 @@ consumer_batch = KafkaConsumer(
     fetch_max_wait_ms=10000
 )    
 
-data = []
-# with open(f"{uuid.uuid4()}.json", 'w') as f:
-with open("test.json", 'w') as f:
-    for message in consumer_batch:
-        data.append((message.value))
-        if len(data) >= 50:
-            print(len(data))
-            print(type(f.name))
-            s3_client.put_object(Body=json.dumps(data, separators=(',', ':'), ensure_ascii=True),
-                                Bucket='pinterest-data-a25f6b34-55e7-4a83-a1ef-4c02a809a2a9',
-                                Key=str(f.name)
-                                )
-            data.clear()
-            break
+
+class batch_to_s3:
+    def __init__(self, kafka_consumer) -> None:
+        self.consumer = kafka_consumer
+
+    
+    def consumer_to_s3(self):
+        """
+        This function reads messages from a Kafka topic cleans characters
+        and writes them to a file in S3 once reaches 50 messages
+        """
+        data = []
+        with open("test.json", 'w') as f:
+            for message in self.consumer:
+                data.append((message.value))
+                if len(data) >= 50:
+                    print(len(data))
+                    print(type(f.name))
+                    s3_client.put_object(Body=json.dumps(data, separators=(',', ':'), ensure_ascii=True),
+                                        Bucket='pinterest-data-a25f6b34-55e7-4a83-a1ef-4c02a809a2a9',
+                                        Key=str(f.name)
+                                        )
+                    print(f'{len(data)} messages have been sent to S3')
+                    data.clear()
+                    break
+
+
+if __name__ == '__main__':
+    batch_consumer = batch_to_s3(consumer_batch)
+    batch_consumer.consumer_to_s3()
--- pinterest_API/API/spark_batch.py
+++ pinterest_API/API/spark_batch.py
@@ -17,20 +17,21 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.amazonaws:aws-java-sdk-s3:1.12.196,org.apache.hadoop:hadoop-aws:3.3.1 pyspark-shell'
 
+
+# Setting the SparkConf for the SparkContext.
 S3_config = (
     pyspark.SparkConf()
     .setAppName('S3app')
 
 )
 
-AWS_KEY = os.environ.get("AWS_ACCESS_KEY")
-AWS_SECRET = os.environ.get("AWS_SECRET_KEY")
-
-
+# Creating a SparkContext object.
 sc_s3 = pyspark.SparkContext(conf=S3_config)
 
-# accessKeyId=os.environ.get('AWS_ACCESS_KEY')
-# secretAccessKey=os.environ.get('AWS_SECRET_KEY')
+
+# Setting up the credentials for the AWS S3 bucket and setting up spark connectors to S3.
+AWS_KEY = os.environ.get("AWS_ACCESS_KEY")
+AWS_SECRET = os.environ.get("AWS_SECRET_KEY")
 hadoopConf = sc_s3._jsc.hadoopConfiguration()
 hadoopConf.set('fs.s3a.access.key', AWS_KEY)
 hadoopConf.set('fs.s3a.secret.key', AWS_SECRET)
@@ -38,31 +39,51 @@ hadoopConf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoo
 
 
 
+# Creating a spark session object.
 s3_spark = SparkSession(sc_s3)
 
-def ascii_ignore(x):
-    return x.encode('ascii', 'ignore').decode('ascii')
-
-ascii_udf = udf(ascii_ignore)
-
-df = s3_spark.read.json("s3a://pinterest-data-a25f6b34-55e7-4a83-a1ef-4c02a809a2a9/test.json")
-# df = ps.DataFrame(df)
-cols_to_cast = ['category', 'unique_id', 'title', 'description', 'follower_count', 'tag_list', 'is_image_or_video', 'image_src', 'save_location']
-df = df.select([col(c).cast(StringType()) if c in cols_to_cast else c for c in df.columns])
-df = df.withColumn("tag_list",regexp_replace(col("tag_list"), ",", ""))
-df = df.withColumn("description",regexp_replace(col("description"), "#", ""))
-df.withColumn("tag_list", ascii_udf('tag_list'))
-df.withColumn("tag_list", ascii_udf('tag_list'))
-df.withColumn("title", ascii_udf('title'))
-df.withColumn("description", ascii_udf('description'))
-df = df.withColumn("follower_count",regexp_replace(col("follower_count"), "k", "000"))
-df = df.withColumn("follower_count",regexp_replace(col("follower_count"), "M", "000000"))
-df = df.withColumn("follower_count",regexp_replace(col("follower_count"), "User Info Error", "0"))
-int_cols_to_cast = [ 'follower_count', 'downloaded', 'index']
-df = df.select([col(c).cast(IntegerType()) if c in int_cols_to_cast else c for c in df.columns])
+class spark_cleaning:
+    def __init__(self, s3Bucket_url: str) -> None:
+        self.url = s3Bucket_url
+        # spark cannot directly understand python functions so they need to be wrapped in udf
+        self.ascii_udf = udf(self.ascii_ignore)
 
 
-df.show()
-df.printSchema()
+    def ascii_ignore(self, x):
+        """
+        It takes a string and returns a string with all non-ascii characters removed
+        
+        :param x: The string to be encoded
+        :return: the string x, encoded as ascii, ignoring any non-ascii characters.
+        """
+        return x.encode('ascii', 'ignore').decode('ascii')
+
+# Creating a user defined function that will be used to remove non-ascii characters from the
+# dataframe. spark cannot directly understand python functions so they need to be wrapped in udf
+
+    def cleaning_transformations(self):
+        df = s3_spark.read.json(path=self.url)
+        cols_to_cast = ['category', 'unique_id', 'title', 'description', 'tag_list', 'is_image_or_video', 'image_src', 'save_location']
+        df = df.select([col(c).cast(StringType()) if c in cols_to_cast else c for c in df.columns])
+        df = df.withColumn("tag_list",regexp_replace(col("tag_list"), ",", ""))
+        df = df.withColumn("description",regexp_replace(col("description"), "#", ""))
+        df = df.withColumn("tag_list", self.ascii_udf('tag_list'))
+        df = df.withColumn("tag_list", self.ascii_udf('tag_list'))
+        df = df.withColumn("title", self.ascii_udf('title'))
+        df = df.withColumn("description", self.ascii_udf('description'))
+        df = df.withColumn("follower_count",regexp_replace(col("follower_count"), "k", "000"))
+        df = df.withColumn("follower_count",regexp_replace(col("follower_count"), "M", "000000"))
+        df = df.withColumn("follower_count",regexp_replace(col("follower_count"), "User Info Error", "0"))
+        int_cols_to_cast = [ 'follower_count', 'downloaded', 'index']
+        df = df.select([col(c).cast(IntegerType()) if c in int_cols_to_cast else c for c in df.columns])
+        df.show()
+        df.printSchema()
+        return df
+
+
+
+if __name__ == '__main__':
+    s3_spark = spark_cleaning(s3Bucket_url='s3a://pinterest-data-a25f6b34-55e7-4a83-a1ef-4c02a809a2a9/test.json')
+    s3_spark.cleaning_transformations()
 
 #%%
